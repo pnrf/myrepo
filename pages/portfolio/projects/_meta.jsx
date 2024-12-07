a4c812d42ff99336a28a8939d40bdc05f51ae99e
Pankratov <pnrf@yandex.ru>
2024-12-07 16:10:43 +0300

    build: migrate to Nextra 3.x

---
+++ pages/portfolio/projects/_meta.jsx
@@ -0,0 +1,47 @@
+export default {
+	'*': {
+		display: 'hidden'
+	},
+	'registratsiya-rossiyskogo-po-v-reyestre-mintsifry': {
+		title:  'Реестр ПО Минцифры'
+	},
+	'registratsiya-programm-dlya-evm-v-rospatente': {
+		title: 'Реестр ЭВМ Роспатента'
+	},
+	'registratsiya-po-v-roszdravnadzore': {
+		title: 'Реестр медизделий'
+	},
+	'sostavleniye-zayavok-na-polucheniye-grantov': {
+		title: 'Заявки на гранты'
+	},
+	'sostavleniye-dokumentov-dlya-uchastiya-v-goszakupkakh': {
+		title: 'Документация для госзакупок'
+	},
+	'napisaniye-ui-tekstov-i-kontenta-dlya-baz-znaniy': {
+		title: 'UI-тексты и база знаний'
+	},
+	'oformleniye-sbornika-istoriy-tekhpisateley': {
+		title: 'Сборник историй'
+	},
+	'perevod-dokumentatsii-nextra': {
+		title: 'Nextra 2.x'
+	},
+	'razrabotka-saita-portfolio-tekhpisatelya': {
+		title: 'Сайт-портфолио'
+	},
+	'uchastiye-v-obshchestvennykh-meropriyatiyakh': {
+		title: 'Мероприятия'
+	},
+	'uchastiye-v-razrabotke-zakonoproyektov': {
+		title: 'Законопроекты'
+	},
+	'obucheniye-veb-razrabotke': {
+		title: 'Веб-разработка'
+	},
+	'websites-layout': {
+		title: 'Вёрстка веб-сайтов'
+	},
+	'razrabotka-sayta-dlya-tekhnicheskikh-pisateley': {
+		title: 'Разработка сайта для технических писателей'
+	}
+}
