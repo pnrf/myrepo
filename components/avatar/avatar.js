d542a0430fc629210b19adb27328e95f5e187c43
Pankratov <pnrf@yandex.ru>
2024-12-24 23:30:23 +0300

    build: update content

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -2,6 +2,6 @@ import styles from './avatar.module.css'
 
 export default function Avatar() {
   return (
-		<img src='https://github.com/pnrf/myrepo/blob/main/images/yup-avatar-x400-2.png' alt='аватарка' className={styles.avatar}/>
+		<img src='https://github.com/pnrf/myrepo/blob/main/images/yup-avatar-x400-2.png?raw=true' alt='аватарка' className={styles.avatar}/>
   )
 }
