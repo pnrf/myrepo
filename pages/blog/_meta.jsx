e919ca03d11b2e5e3a38406f5ea5984eb0d0b13b
Pankratov <pnrf@yandex.ru>
2024-12-26 15:07:01 +0300

    docs: update content

--- pages/blog/_meta.jsx
+++ pages/blog/_meta.jsx
@@ -3,15 +3,6 @@ export default {
 	'---': {
     type: 'separator'
   },
-	'-- Articles': {
-    type: 'separator',
-    title: 'Статьи'
-  },
 	'how-create-website-with-documentation': 'Как создать сайт с документацией',
-	'zachem-tekhpisatelyu-sayt-portfolio': 'Зачем техписателю сайт-портфолио',
-	'-- Notes': {
-    type: 'separator',
-    title: 'Заметки'
-  },
-	'poiskovaya-optimizatsiya-servisy-yandex': 'Поисковая оптимизация. Сервисы Яндекса'
+	'zachem-tekhpisatelyu-sayt-portfolio': 'Зачем техписателю сайт-портфолио'
 }
--- pages/portfolio/_meta.jsx
+++ pages/portfolio/_meta.jsx
@@ -5,10 +5,6 @@ export default {
   },
 	projects: 'Проекты',
 	showcases: 'Документы',
-	manual_link: {
-    title: 'Инструкции',
-    href: 'https://www.myrepo.ru/blog#инструкции'
-  },
 	storybook_link: {
     title: 'UI-компоненты ↗',
     href: '#'
