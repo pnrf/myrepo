ea64624e686ac65e2e511cb595e1ffe16b7c2b13
Pankratov <pnrf@yandex.ru>
2025-01-03 11:11:15 +0300

    docs: update content

---
+++ components/main-page/main-page.jsx
@@ -0,0 +1,22 @@
+import styles from './main-page.module.css'
+
+export default function MainPage() {
+  return (
+		<div>
+			<div>
+				<div>
+					<h1>Сайт-портфолио технического писателя</h1>
+					<p>Пишу о себе и о применяемых в работе технологиях</p>
+					<a href='https://www.myrepo.ru/blog'>Читайте в блоге</a>
+				</div>
+				<div>
+					<div>
+						<ul>
+							<li></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+  )
+}
