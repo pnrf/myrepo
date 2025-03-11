de57379911bcf0af86aef3c2bbc26ee856c47510
Pankratov <pnrf@yandex.ru>
2025-03-11 22:32:40 +0300

    refactor: transfer inline styles to .module.css

--- components/main-page/main-page.jsx
+++ components/main-page/main-page.jsx
@@ -19,25 +19,25 @@ export default function MainPage() {
 					<img className={styles['hero-section__image']} src="https://github.com/pnrf/myrepo/blob/main/images/portfolioPics/hero-section/yup-hero-section-02.png?raw=true" alt="Фоновое фото" />
 				</div>
 			</section>
-			<h2 style={{fontSize: "40px", color: "#647F94", paddingBottom: "20px"}}>Портфолио</h2>
-			<section style={{display: "grid", gridTemplateColumns: "2fr 3fr auto", gap: "2em"}}>
-				<div style={{display: "flex", flexDirection: "column", backgroundColor: "#647F94", color: "whitesmoke", border: "1px solid #647F94", borderRadius: "8px", padding: ".75rem 1.5rem", marginTop: ".5rem"}}>
-					<h2 style={{fontSize: "1.75em", fontWeight: "600", lineHeight: "1.75em", paddingBottom: "8px"}}>Обо мне</h2>
-					<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%"}}>
-						<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-							<p style={{paddingBottom: "20px"}}>Работаю в ИТ с 2023 года.</p> 
-							<p style={{paddingBottom: "20px"}}>Разрабатываю техническую и юридическую документацию по программным продуктам.</p>
-							<p style={{paddingBottom: "20px"}}>Пишу тексты для интерфейсов и создаю контент для корпоративных баз знаний.</p>
-							<p style={{paddingBottom: "40px"}}>Изучаю ГОСТ 19 и 34 серий. Общаюсь с сообществом.</p>
+			<h2 className={styles['section__title']}>Портфолио</h2>
+			<section className={styles['portfolio-section']}>
+				<div className={styles['portfolio-section__about']}>
+					<h2 className={styles['portfolio-section__card-title']}>Обо мне</h2>
+					<div className={styles['portfolio-section__content']}>
+						<div className={styles['portfolio-section__text-wrapper']}>
+							<p className={styles['portfolio-section__text']}>Работаю в ИТ с 2023 года.</p> 
+							<p className={styles['portfolio-section__text']}>Разрабатываю техническую и юридическую документацию по программным продуктам.</p>
+							<p className={styles['portfolio-section__text']}>Пишу тексты для интерфейсов и создаю контент для корпоративных баз знаний.</p>
+							<p className={styles['portfolio-section__text']}>Изучаю ГОСТ 19 и 34 серий. Общаюсь с сообществом.</p>
 						</div>
-						<a href="/about">Подробнее <span>→</span></a>
+						<a className={styles['portfolio-section__button']} href="/about">Подробнее →</a>
 					</div>
 				</div>
-				<div style={{display: "flex", flexDirection: "column", border: "1px solid #647F94", borderRadius: "8px", padding: ".75rem 1.5rem", marginTop: ".5rem"}}>
-					<h2 style={{fontSize: "1.75em", fontWeight: "600", lineHeight: "1.75em", paddingBottom: "8px"}}>Проекты</h2>
-					<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%"}}>
-						<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-							<ul style={{listStyle: "inside"}}>
+				<div className={styles['portfolio-section__white-card']}>
+					<h2 className={styles['portfolio-section__card-title']}>Проекты</h2>
+					<div className={styles['portfolio-section__content']}>
+						<div className={styles['portfolio-section__text-wrapper']}>
+							<ul className={styles['portfolio-section__card-list']}>
 								<li><a href="/portfolio/projects/registratsiya-rossiyskogo-po-v-reyestre-mintsifry">Регистрация российского ПО в реестре Минцифры</a></li>
 								<li><a href="/portfolio/projects/registratsiya-programm-dlya-evm-v-rospatente">Регистрация программ для ЭВМ в Роспатенте</a></li>
 								<li><a href="/portfolio/projects/registratsiya-po-v-roszdravnadzore">Регистрация ПО в Росздравнадзоре</a></li>
@@ -45,27 +45,29 @@ export default function MainPage() {
 								<li><a href="/portfolio/projects/sostavleniye-dokumentov-dlya-uchastiya-v-goszakupkakh">Составление документов для участия в госзакупках</a></li>
 							</ul>
 						</div>
-						<a href="/portfolio/projects">Все проекты <span>→</span></a>
+						<a href="/portfolio/projects">Все проекты →</a>
 					</div>
 				</div>
-				<div style={{display: "flex", flexDirection: "column", border: "1px solid #647F94", borderRadius: "8px", padding: ".75rem 1.5rem", marginTop: ".5rem"}}>
-					<h2 style={{fontSize: "1.75em", fontWeight: "600", lineHeight: "1.75em", paddingBottom: "8px"}}>Документы</h2>
-					<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%"}}>
-						<div style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-							<ul style={{listStyle: "inside"}}>
-							<li><a href="#">Руководство пользователя (ГОСТ 19)</a></li>
-							<li><a href="#">Технические условия на ПО (ГОСТ 19)</a></li>
-							<li><a href="#">Бриф для составления ТЗ на разработку ИИ-сервиса</a></li>
-							<li><a href="#">Краткое описание программного комплекса</a></li>
-							<li><a href="#">Назначение использования ПО и перечень бизнес-функций</a></li>
-							<li><a href="#">Описание ПО для подачи заявки на грант</a></li>
+				<div className={styles['portfolio-section__white-card']}>
+					<h2 className={styles['portfolio-section__card-title']}>Документы</h2>
+					<div className={styles['portfolio-section__content']}>
+						<div className={styles['portfolio-section__text-wrapper']}>
+							<ul className={styles['portfolio-section__card-list']}>
+								<li><a href="#">Руководство пользователя (ГОСТ 19)</a></li>
+								<li><a href="#">Технические условия на ПО (ГОСТ 19)</a></li>
+								<li><a href="#">Бриф для составления ТЗ на разработку ИИ-сервиса</a></li>
+								<li><a href="#">Краткое описание программного комплекса</a></li>
+								<li><a href="#">Назначение использования ПО и перечень бизнес-функций</a></li>
+								<li><a href="#">Описание ПО для подачи заявки на грант</a></li>
 							</ul>
 						</div>
-						<a href="/portfolio/documents">Все документы <span>→</span></a>
+						<a href="/portfolio/documents">Все документы →</a>
 					</div>
 				</div>
 			</section>
-			<h2 style={{fontSize: "40px", color: "#647F94", marginTop: "40px"}}>Блог</h2>
+			<section className={styles['blog-section']}>
+				<h2 className={styles['section__title']}>Блог</h2>
+			</section>
 		</div>
   )
 }
