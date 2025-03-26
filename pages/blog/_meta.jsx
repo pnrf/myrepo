64c6a420bb0765ff4f41023f93884fa63d85ec2a
Pankratov <pnrf@yandex.ru>
2025-03-26 23:30:56 +0300

    docs: update content

--- pages/blog/_meta.jsx
+++ pages/blog/_meta.jsx
@@ -9,6 +9,7 @@ export default {
 	'---': {
     type: 'separator'
   },
+	'kak-raskrepostit-techsoobshchestvo': 'Как раскрепостить техническое сообщество',
 	'problemy-v-poiske-raboty-chto-nuzhno-znat-nachinayushchemu-tekhpisatelyu': 'Проблемы в поиске работы. Что нужно знать начинающему техписателю?',
 	'how-create-website-with-documentation': 'Как создать сайт с документацией',
 	'zachem-tekhpisatelyu-sayt-portfolio': 'Зачем техписателю сайт-портфолио'
